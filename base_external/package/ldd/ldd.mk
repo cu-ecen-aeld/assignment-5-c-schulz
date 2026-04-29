ab58958746dd102e70c2b721f6615d68194c1bcb
Cornelia Schulz <cornelia.schulz@web.de>
2026-04-29 14:42:24 +0200

    add ldd3 package

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,30 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+# assignment 7) reference your assignment 7 git contents
+LDD_VERSION = 'da1d3850f69ee2d56121b530b694d03385cc2426'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-c-schulz.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+# assignment 7) build the misc-modules and scull components
+LDD3_MODULE_SUBDIRS = misc-modules scull
+# define LDD_BUILD_CMDS
+#  	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
+#  	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull all
+# endef
+
+# assignment 7) install the misc-modules and scull components into the root filesystem
+define LDD_INSTALL_TARGET_CMDS
+    $(INSTALL) -m 0755 $(@D)/misc-modules/module_load 	$(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/scull/scull_load 			$(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/scull/scull_unload 		$(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
